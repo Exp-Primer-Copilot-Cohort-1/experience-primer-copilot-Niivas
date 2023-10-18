db736ee94d21d1e9450bc125a1747371f88f6585
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-10-18 05:35:11 +0000

    Co-pilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+import { createServer } from 'http';
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
