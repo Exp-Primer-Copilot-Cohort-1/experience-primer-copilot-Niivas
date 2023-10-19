7eaaaf1f7df76e28ad96f46525618326d1ee2e18
Nivas Reddy Nalla <nivas6900@gmail.com>
2023-10-19 06:28:29 +0000

    Added a new function

--- comments.js
+++ comments.js
@@ -9,3 +9,19 @@ const server = createServer((req, res) => {
 server.listen(3000, () => {
     console.log('Server running on port 3000');
 });
+
+import https from 'https';
+
+function getProjects() {
+    https.get('https://niivas.github.io/project', (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            console.log(data);
+        });
+    }).on('error', (err) => {
+        console.log('Error: ' + err.message);
+    });
+}
